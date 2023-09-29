0300e64079a41c09e42877329b7a7816e7ff4881
Dino Kotaranin <dino.kotaranin@gmail.com>
2023-09-29 22:06:43 +0200

    backend controller edit

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -11,63 +11,110 @@ import axios from "axios";
 export const getInsurancePrice = async (req: Request, res: Response) => {
   const { mainForm, discounts, coverages } = req.body;
 
-  const customerAge = calculateAge(mainForm.birthdate);
-  const ageConsant = getConstantByAge(customerAge);
-
-  const cityPopulation = await getCityPopulation(mainForm.city);
-
-  if (cityPopulation.error) {
-    return res.status(Number(cityPopulation.status)).send(cityPopulation.error);
-  }
-  const basePrice = 100; /*calculateBasePrice(
-    ageConsant,
-    Number(cityPopulation.cityPopulation)
-  );*/
-
-  const voucher = Number(mainForm.voucher);
-
-  const { bonusProtection, aoPlus, glassProtection } = calculateCoverages(
-    basePrice,
-    customerAge,
-    mainForm.vehiclePower,
-    coverages
-  );
-
-  const {
-    commercialDiscount,
-    adviserDiscount,
-    vipDiscount,
-    strongCarSurcharge,
-    totalPrice,
-  } = calculateDiscountsAndTotalPrice(
-    basePrice,
-    voucher,
-    discounts,
-    bonusProtection,
-    aoPlus,
-    glassProtection
-  );
-
-  const finalData = {
+  var finalData = {
     coveragePrices: {
-      bonusProtection,
-      aoPlus,
-      glassProtection,
+      bonusProtection: 0,
+      aoPlus: 0,
+      glassProtection: 0,
     },
     discountPrices: {
+      commercialDiscount: 0,
+      adviserDiscount: 0,
+      vipDiscount: 0,
+      strongCarSurcharge: 0,
+    },
+    insurancePrices: {
+      basePrice: 0,
+      totalPrice: 0,
+      voucher: 0,
+    },
+  };
+
+  if (mainForm.priceMatch === "") {
+    const customerAge = calculateAge(mainForm.birthdate);
+    const ageConsant = getConstantByAge(customerAge);
+
+    const cityPopulation = await getCityPopulation(mainForm.city);
+
+    if (cityPopulation.error) {
+      return res
+        .status(Number(cityPopulation.status))
+        .send(cityPopulation.error);
+    } else if (cityPopulation.cityPopulation === "0") {
+      return res.status(404).send("There is no data for provided city");
+    }
+
+    const basePrice = calculateBasePrice(
+      ageConsant,
+      Number(cityPopulation.cityPopulation)
+    );
+
+    if (basePrice === 0) {
+      return res.status(400).send("User must be at least 18 years old");
+    }
+
+    const voucher = Number(mainForm.voucher);
+    const { bonusProtection, aoPlus, glassProtection } = calculateCoverages(
+      basePrice,
+      customerAge,
+      mainForm.vehiclePower,
+      coverages
+    );
+
+    const {
       commercialDiscount,
       adviserDiscount,
       vipDiscount,
       strongCarSurcharge,
-    },
-    insurancePrices: {
-      basePrice,
       totalPrice,
+    } = calculateDiscountsAndTotalPrice(
+      basePrice,
       voucher,
-    },
-  };
+      discounts,
+      bonusProtection,
+      aoPlus,
+      glassProtection
+    );
 
-  console.log(finalData);
+    finalData = {
+      coveragePrices: {
+        bonusProtection,
+        aoPlus,
+        glassProtection,
+      },
+      discountPrices: {
+        commercialDiscount,
+        adviserDiscount,
+        vipDiscount,
+        strongCarSurcharge,
+      },
+      insurancePrices: {
+        basePrice,
+        totalPrice,
+        voucher,
+      },
+    };
+  } else {
+    const basePrice = +(
+      mainForm.priceMatch /
+      (1 +
+        (coverages.bonusProtection * 12) / 100 -
+        (discounts.commercialDiscount * 10) / 100)
+    ).toFixed(2);
+    const bonusProtection = coverages.bonusProtection
+      ? +((basePrice * 12) / 100).toFixed(2)
+      : 0;
+    const commercialDiscount = discounts.commercialDiscount
+      ? +((basePrice * 10) / 100).toFixed(2)
+      : 0;
+
+    finalData.coveragePrices.bonusProtection = bonusProtection;
+    finalData.insurancePrices.basePrice = basePrice;
+    finalData.discountPrices.commercialDiscount = commercialDiscount;
+    finalData.insurancePrices.totalPrice = Number(mainForm.priceMatch);
+    console.log(finalData);
+    //res.send(finalData);
+  }
   res.send(finalData);
 };
 
@@ -75,21 +122,27 @@ const getCityPopulation = async (
   city: string
 ): Promise<{ [key: string]: string }> => {
   try {
-    const populationResponse = await axios.post(
-      "https://countriesnow.space/api/v0.1/countries/population/cities",
-      { city: city },
+    const populationResponse = await axios.get(
+      `https://api.api-ninjas.com/v1/city?name=${city}`,
       {
         headers: {
           "Content-Type": "application/json",
+          "x-api-key": "TV8D326FoECzom44sqmBwg==yquXqD3ZS6bH8r14",
         },
       }
     );
-    const cityPopulation =
-      populationResponse.data.data.populationCounts[0].value;
-    return {
-      cityPopulation,
-    };
+    if (populationResponse.data[0]) {
+      const cityPopulation = populationResponse.data[0].population;
+      return {
+        cityPopulation,
+      };
+    } else {
+      return {
+        cityPopulation: "0",
+      };
+    }
   } catch (error: any) {
-    return { error: error.response.data.msg, status: error.response.status };
+    const err = error.response.data.error;
+    return { error: err, status: error.response.status };
   }
 };
