a1b27a85c1213275010e5f339dd16f698948820c
Dino Kotaranin <dino.kotaranin@gmail.com>
2023-09-29 11:35:25 +0200

    calculations component edit

--- frontend/src/components/Calculations/index.tsx
+++ frontend/src/components/Calculations/index.tsx
@@ -1,53 +1,83 @@
 import React from "react";
-import {
-  Box,
-  Grid,
-  Typography,
-  List,
-  ListItem,
-  ListItemText,
-} from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import { useInsuranceContext } from "../../InsuranceContext";
 
 const Calculations: React.FC = () => {
   const { discountPrices, coveragePrices, insurancePrices } =
     useInsuranceContext();
 
-  function generate(element: React.ReactElement) {
-    return [0, 1, 2].map((value) =>
-      React.cloneElement(element, {
-        key: value,
-      })
-    );
-  }
-
   return (
-    <Box maxWidth="sm" sx={{ mt: "60px", mb: "40px" }}>
-      <Grid container spacing={2}></Grid>
-      <Typography>Basic price: {insurancePrices.basePrice} EUR</Typography>
-      <Grid item xs={12} md={6}>
-        <Typography sx={{ mt: 2, mb: 1 }}>Discounts:</Typography>
-        <List>
-          {generate(
-            <ListItem>
-              <ListItemText primary="Single-line item" />
-            </ListItem>
+    <Stack direction="column" gap={2} sx={{ mt: "32px", mb: "40px" }}>
+      <Typography>
+        <strong>Base price:</strong> {insurancePrices.basePrice} EUR
+      </Typography>
+      <Box>
+        <Typography>
+          <strong>Discounts / surcharges:</strong>
+        </Typography>
+        <Stack
+          direction="column"
+          gap={1}
+          sx={{ paddingLeft: "16px", marginTop: "8px" }}
+        >
+          {discountPrices.commercialDiscount !== 0 && (
+            <Typography>
+              Commercial discount: {discountPrices.commercialDiscount} EUR
+            </Typography>
           )}
-        </List>
-      </Grid>
-      <Grid item xs={12} md={6}>
-        <Typography sx={{ mt: 2, mb: 1 }}>Coverages:</Typography>
-        <List>
-          {generate(
-            <ListItem>
-              <ListItemText primary="Single-line item" />
-            </ListItem>
+          {discountPrices.adviserDiscount !== 0 && (
+            <Typography>
+              Adviser discount: {discountPrices.adviserDiscount} EUR
+            </Typography>
           )}
-        </List>
-      </Grid>
-      <Typography>Total price: {insurancePrices.totalPrice} EUR</Typography>
-      <Grid />
-    </Box>
+          {discountPrices.vipDiscount !== 0 && (
+            <Typography>
+              VIP discount: {discountPrices.vipDiscount} EUR
+            </Typography>
+          )}
+
+          {discountPrices.strongCarSurcharge !== 0 && (
+            <Typography>
+              Strong car surcharge: {discountPrices.strongCarSurcharge} EUR
+            </Typography>
+          )}
+        </Stack>
+      </Box>
+      <Box>
+        <Typography>
+          <strong>Coverages:</strong>
+        </Typography>
+        <Stack
+          direction="column"
+          gap={1}
+          sx={{ paddingLeft: "16px", marginTop: "8px" }}
+        >
+          {coveragePrices.bonusProtection !== 0 && (
+            <Typography>
+              Bonus protection: {coveragePrices.bonusProtection} EUR
+            </Typography>
+          )}
+          {coveragePrices.aoPlus !== 0 && (
+            <Typography>AO+: {coveragePrices.aoPlus} EUR</Typography>
+          )}
+          {coveragePrices.glassProtection !== 0 && (
+            <Typography>
+              Glass protection: {coveragePrices.glassProtection} EUR
+            </Typography>
+          )}
+        </Stack>
+      </Box>
+      <Box>
+        <Typography sx={{ disaplay: "inline" }}>
+          <strong>Total price:</strong> {insurancePrices.totalPrice} EUR
+          {insurancePrices.voucher > 0 && (
+            <Typography display="inline" ml={2} fontSize={14}>
+              ({insurancePrices.voucher} EUR voucher included)
+            </Typography>
+          )}
+        </Typography>
+      </Box>
+    </Stack>
   );
 };
 
